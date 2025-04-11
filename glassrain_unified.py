46b7de9a0cb97c1d286db9550a50ebafcaf67cf9
Clae Henderson <claehenderson@Claes-Air.lan>
2025-04-10 20:07:14 -0400

    Fix: changed landing.html to index.html in index route

--- glassrain_unified.py
+++ glassrain_unified.py
@@ -68,7 +68,7 @@ init_ai_routes(app)
 @app.route('/')
 def index():
     """Simple HTML status page for GlassRain"""
-    return render_template('landing.html')
+    return render_template('index.html')
 
 @app.route('/api/status')
 def status():
